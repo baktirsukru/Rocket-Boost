adabd59368c43386ce0e2605261cedfa8e47cc33
baktirsukru <101118829+baktirsukru@users.noreply.github.com>
2024-11-17 19:32:25 +0300

    sfx

--- Assets/Scripts/Collision Handler.cs
+++ Assets/Scripts/Collision Handler.cs
@@ -1,31 +1,65 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 0.5f;
+    [SerializeField] AudioClip finishAudio;
+    [SerializeField] AudioClip crashAudio;
+
+    AudioSource audioSource;
+
+    bool isControllable = true;
+
+    private void Start() 
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     
-    
-        private void OnCollisionEnter(Collision other) 
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(!isControllable) {return;}
+        
+        switch(other.gameObject.tag)
         {
-            switch(other.gameObject.tag)
-            {
-                case "Friendly":
-                    Debug.Log("all good");
-                    break;
-                case "Finish":
-                    LoadNextLevel();
-                    break;
-                case "Fuel":
-                    Debug.Log("fuel added");
-                    break;
-                default:
-                    ReloadLevel();
-                    break;
-            }
+            case "Friendly":
+                Debug.Log("all good");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        
         }
-    
-    
+
+    }
+
+    private void StartSuccessSequence()
+    {
+        //todo: add particle effect
+        isControllable = false;
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+        audioSource.Stop();
+        audioSource.PlayOneShot(finishAudio);
+
+        
+    }
+
+    private void StartCrashSequence()
+    {
+        //todo: add particle effect
+        isControllable = false;
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay); 
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashAudio);
+    }
+
     private void LoadNextLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
@@ -38,12 +72,10 @@ public class CollisionHandler : MonoBehaviour
 
         SceneManager.LoadScene(nextScene);
     }
+    
     private void ReloadLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
-    }
-    
-
-    
+    }   
 }
